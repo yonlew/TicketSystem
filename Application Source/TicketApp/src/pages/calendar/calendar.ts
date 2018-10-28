359eb5bfaa89ab0745783606698990d0708a4662
yoni lew <u13318765@tuks.co.za>
2018-10-28 11:31:27 +0200

    Added Calendar

---
+++ Application Source/TicketApp/src/pages/calendar/calendar.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the CalendarPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-calendar',
+  templateUrl: 'calendar.html',
+})
+export class CalendarPage {
+
+  date: any;
+  daysInThisMonth: any;
+  daysInLastMonth: any;
+  daysInNextMonth: any;
+  monthNames: string[];
+  currentMonth: any;
+  currentYear: any;
+  currentDate: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  getDaysOfMonth() {
+    this.daysInThisMonth = new Array();
+    this.daysInLastMonth = new Array();
+    this.daysInNextMonth = new Array();
+    this.currentMonth = this.monthNames[this.date.getMonth()];
+    this.currentYear = this.date.getFullYear();
+    if (this.date.getMonth() === new Date().getMonth()) {
+      this.currentDate = new Date().getDate();
+    } else {
+      this.currentDate = 999;
+    }
+
+    var firstDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 1).getDay();
+    var prevNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth(), 0).getDate();
+    for (var i = prevNumOfDays - (firstDayThisMonth - 1); i <= prevNumOfDays; i++) {
+      this.daysInLastMonth.push(i);
+    }
+
+    var thisNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getDate();
+    for (i = 0; i < thisNumOfDays; i++) {
+      this.daysInThisMonth.push(i + 1);
+    }
+
+    var lastDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getDay();
+    var nextNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth() + 2, 0).getDate();
+    for (i = 0; i < (6 - lastDayThisMonth); i++) {
+      this.daysInNextMonth.push(i + 1);
+    }
+    var totalDays = this.daysInLastMonth.length + this.daysInThisMonth.length + this.daysInNextMonth.length;
+    if (totalDays < 36) {
+      for (i = (7 - lastDayThisMonth); i < ((7 - lastDayThisMonth) + 7); i++) {
+        this.daysInNextMonth.push(i);
+      }
+    }
+  }
+
+  goToLastMonth() {
+    this.date = new Date(this.date.getFullYear(), this.date.getMonth(), 0);
+    this.getDaysOfMonth
+  }
+
+  goToNextMonth() {
+    this.date = new Date(this.date.getFullYear(), this.date.getMonth() + 2, 0);
+    this.getDaysOfMonth
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CalendarPage');
+  }
+
+}
--- Application Source/TicketApp/src/pages/home/home.ts
+++ Application Source/TicketApp/src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CalendarPage } from '../calendar/calendar';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  // calendar: CalendarPage;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // this.calendar = new CalendarPage();
   }
 
+  anOtherPage() {
+    this.navCtrl.push(CalendarPage);
+  }
 }
